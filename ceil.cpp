8829cdf69006651288f2819069e1cb0d4d19fcd2
RohanS00007 <rohansingh70112@gmail.com>
2024-10-12 10:16:32 +0530

    Add files via upload

---
+++ ceil.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int ceil(int arr[],int n,int target){
+    int low=0;
+    int high=n-1;
+    int ans=n;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(arr[mid]>=target){ 
+            ans=arr[mid];
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+        
+    }
+    return ans;
+    
+    
+    
+}
+int main(){
+    int arr[]={10,20,30,40,50};
+    int size=5;
+    int x=25;
+    cout<<ceil(arr,size,x);  
+}
